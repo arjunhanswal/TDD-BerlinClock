91f5d8a1e0b0e85d77bb9b5eb013b57e8d88cfb8
arjun <arjunhanswal@gmail.com>
2024-01-11 21:29:17 +0530

    initial project setup

---
+++ TDD-BerlinClock/TDD-BerlinClock/BerlinClockView.swift
@@ -0,0 +1,14 @@
+
+import SwiftUI
+
+struct BerlinClockView: View {
+    var body: some View {
+        Text("Berlin Clock!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        BerlinClockView()
+    }
+}
